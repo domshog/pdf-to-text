2ea99c2598717bd5c1678caafc5bf6063e621c1d
Mohammed QASEM <45571793+domshog@users.noreply.github.com>
2024-04-29 11:23:42 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import os
-import pytesseract
 import fitz  # PyMuPDF
-
-# Path to your Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf'}
@@ -16,11 +12,14 @@ def allowed_file(filename):
 
 def extract_text(pdf_file):
     text_output = []
-    with fitz.open(pdf_file) as doc:
-        for page_num in range(len(doc)):
-            page = doc.load_page(page_num)
-            text = page.get_text()
-            text_output.append(text)
+    try:
+        with fitz.open(pdf_file) as doc:
+            for page_num in range(len(doc)):
+                page = doc.load_page(page_num)
+                text = page.get_text()
+                text_output.append(text)
+    except Exception as e:
+        st.error(f"Error extracting text: {e}")
     return '\n'.join(text_output)
 
 def main():
@@ -34,8 +33,11 @@ def main():
             with open(file_path, "wb") as f:
                 f.write(uploaded_file.getbuffer())
             text_output = extract_text(file_path)
-            st.subheader("Extracted Text:")
-            st.text_area("Text Output", text_output, height=400)
+            if text_output:
+                st.subheader("Extracted Text:")
+                st.text_area("Text Output", text_output, height=400)
+            else:
+                st.warning("No text found in the PDF file.")
         else:
             st.error("Please upload a PDF file.")
 
