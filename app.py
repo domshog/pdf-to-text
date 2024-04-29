b73c9ecde75e54c4b377cea8dbdc4b6ee77bf5e9
Mohammed QASEM <45571793+domshog@users.noreply.github.com>
2024-04-29 11:03:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
-
+import streamlit as st
 import pytesseract
 from PIL import Image
 from pdf2image import convert_from_path
